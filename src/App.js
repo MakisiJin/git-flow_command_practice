686f624f8562500efbe7a67d293a5405c18395ec
MakisiJin <15735294838@163.com>
2019-04-26 19:11:10 +0800

    increase code eslint

--- src/App.js
+++ src/App.js
@@ -1,24 +1,115 @@
 import React, { Component } from 'react';
+import { Table } from 'antd';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    const dataSource = [];
+    const columns = [{
+      title: '姓名',
+      key: 'name',
+      dataIndex: 'name',
+      render: text => text || '--'
+    }, {
+      title: '性别',
+      key: 'name',
+      dataIndex: 'name',
+      render: text => text || '--'
+    }, {
+      title: '年龄',
+      key: 'name',
+      dataIndex: 'name',
+      render: text => text || '--'
+    }, {
+      title: '身高',
+      key: 'name',
+      dataIndex: 'name',
+      render: text => text || '--'
+    }, {
+      title: '体重',
+      key: 'name',
+      dataIndex: 'name',
+      render: text => text || '--'
+    }, {
+      title: '住址',
+      key: 'name',
+      dataIndex: 'name',
+      render: text => text || '--'
+    }, {
+      title: '特长',
+      key: 'name',
+      dataIndex: 'name',
+      render: text => text || '--'
+    }, {
+      title: '',
+      key: 'name',
+      dataIndex: 'name',
+      render: text => text || '--'
+    }, {
+      title: '姓名',
+      key: 'name',
+      dataIndex: 'name',
+      render: text => text || '--'
+    }, {
+      title: '姓名',
+      key: 'name',
+      dataIndex: 'name',
+      render: text => text || '--'
+    }, {
+      title: '姓名',
+      key: 'name',
+      dataIndex: 'name',
+      render: text => text || '--'
+    }, {
+      title: '姓名',
+      key: 'name',
+      dataIndex: 'name',
+      render: text => text || '--'
+    }, {
+      title: '姓名',
+      key: 'name',
+      dataIndex: 'name',
+      render: text => text || '--'
+    }, {
+      title: '姓名',
+      key: 'name',
+      dataIndex: 'name',
+      render: text => text || '--'
+    }, {
+      title: '姓名',
+      key: 'name',
+      dataIndex: 'name',
+      render: text => text || '--'
+    }, {
+      title: '姓名',
+      key: 'name',
+      dataIndex: 'name',
+      render: text => text || '--'
+    }, {
+      title: '姓名',
+      key: 'name',
+      dataIndex: 'name',
+      render: text => text || '--'
+    }, {
+      title: '姓名',
+      key: 'name',
+      dataIndex: 'name',
+      render: text => text || '--'
+    }];
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit
+            <code>src/App.js</code> 
+            and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Table
+            dataSource={dataSource}
+            columns={columns}
+          />
         </header>
       </div>
     );
